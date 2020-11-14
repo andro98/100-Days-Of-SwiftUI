a0492c6277cb0ba2de0e2d16c8a6b10c26073707
Andrew Maher <andrewaacodo12@gmail.com>
2020-11-14 02:13:04 +0200

    We Split

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Andrew Maher on 3/3/1737 ERA1.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
